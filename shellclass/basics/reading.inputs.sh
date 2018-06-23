48772567b74f1a88b9eb73729aa6e9f26cb8ce1f
prescottbreeden <prescott.breeden@gmail.com>
2018-06-23 01:12:54 -0700

    rndm passwords

--- shellclass/basics/reading.inputs.sh
+++ shellclass/basics/reading.inputs.sh
@@ -14,8 +14,11 @@ read -p 'Enter the name of the person who this account is for: ' COMMENT
 read -p 'Enter the password to use for the account: ' PASSWORD
 
 # Create the user.
-#sudo useradd 
+useradd -c "${COMMENT}" -m ${USER_NAME}
 
 # Set the password for the user.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
 # Force password change on first login.
+passwd -e ${USER_NAME}
+
